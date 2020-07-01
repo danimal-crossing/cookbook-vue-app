2357f8b668c698f318dae7d14523bd4674975ccd
dani <dani.zaghian@gmail.com>
2020-07-01 10:28:00 -0700

    adds hardcoded recipes and v-for

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios"
+
+// default to using localhost:3000
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <div v-for="recipe in recipes">
+      <h2>Title: {{ recipe.title }}</h2>
+      <p>Prep time: {{ recipe.prep_time }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Hello World",
+      recipes: [
+        {
+          id: 1,
+          title: "Raw Eggs",
+          prep_time: 3
+        },
+        {
+          id: 2,
+          title: "Empty Sandwich",
+          prep_time: 5
+        },
+        {
+          id: 3,
+          title: "Ice Cubes",
+          prep_time: 80
+        }
+      ]
+    };
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
