22fb9f4bef6fdf0f8ea1f6c733499e8f6982b846
dani <dani.zaghian@gmail.com>
2020-07-01 11:42:23 -0700

    adds images with standard width

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,16 @@
     <h1>{{ message }}</h1>
     <div v-for="recipe in recipes">
       <h2>Title: {{ recipe.title }}</h2>
+      <img v-bind:src="recipe.image_url" alt="">
       <p>Prep time: {{ recipe.prep_time }}</p>
     </div>
   </div>
 </template>
 
 <style>
+img {
+  width: 250px;
+}
 </style>
 
 <script>
