8dbe624edfc8393870898de496672a6ee324923a
dani <dani.zaghian@gmail.com>
2020-07-01 12:18:01 -0700

    adds axios post request to create new recipe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>New Recipe</h1>
+    <ul>
+      <li v-for="error in errors">{{ error }}</li>
+    </ul>
+    <button v-on:click="createRecipe()">Create</button>
     <div v-for="recipe in recipes">
       <h2>Title: {{ recipe.title }}</h2>
       <img v-bind:src="recipe.image_url" alt="">
@@ -20,8 +24,8 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Hello World",
-      recipes: []
+      recipes: [],
+      errors: []
     };
   },
   created: function() {
@@ -33,6 +37,26 @@ export default {
         console.log("All Recipes:", response.data);
         this.recipes = response.data;
       });
+    },
+    createRecipe: function() {
+      var params = {
+        title: "example title",
+        ingredients: "example ingredients",
+        directions: "example directions",
+        prep_time: "example prep_time",
+        image_url: "example image_url"
+      };
+
+      axios
+        .post("/api/recipes", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.recipes.push(response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
     }
   }
 };
