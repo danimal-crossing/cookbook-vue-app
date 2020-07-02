5d54c941d813783d6dbd7dea0e3cbf5d7203129f
dani <dani.zaghian@gmail.com>
2020-07-02 11:13:19 -0700

    adds recipes update form in modal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <h1>New Recipe</h1>
     <ul>
-      <li v-for="error in errors">{{ error }}</li>
+      <li v-for="error in createErrors">{{ error }}</li>
     </ul>
 
     <div>
@@ -24,11 +24,15 @@
     <dialog id="recipe-details">
       <form method="dialog">
         <h1>Recipe Info</h1>
-        <p>Title: {{ currentRecipe.title }}<p>
-        <img v-bind:src="currentRecipe.image_url" alt="">
-        <p>Directions: {{ currentRecipe.directions }}</p>
-        <p>Ingredients: {{ currentRecipe.ingredients }}</p>
-        <p>Prep Time: {{ currentRecipe.prep_time }}</p>
+        <!-- <ul>
+          <li v-for="error in updateErrors">{{ error }}</li>
+        </ul> -->
+        <p>Title: <input type="text" v-model="currentRecipe.title"></p>
+        <p>Ingredients: <input type="text" v-model="currentRecipe.ingredients"></p>
+        <p>Directions: <input type="text" v-model="currentRecipe.directions"></p>
+        <p>Prep Time: <input type="text" v-model="currentRecipe.prep_time"></p>
+        <p>Image Url: <input type="text" v-model="currentRecipe.image_url"></p>
+        <button v-on:click="updateRecipe(currentRecipe)">Update</button>
         <button>Close</button>
       </form>
     </dialog>
@@ -48,7 +52,8 @@ export default {
   data: function() {
     return {
       recipes: [],
-      errors: [],
+      createErrors: [],
+      // updateErrors: [],
       newRecipeTitle: "",
       newRecipeIngredients: "",
       newRecipeDirections: "",
@@ -83,13 +88,31 @@ export default {
         })
         .catch(error => {
           console.log(error.response.data.errors);
-          this.errors = error.response.data.errors;
+          this.createErrors = error.response.data.errors;
         });
     },
     showRecipe: function(recipe) {
       console.log(recipe);
       this.currentRecipe = recipe;
       document.querySelector("#recipe-details").showModal();
+    },
+    updateRecipe: function(recipe) {
+      var params = {
+        title: recipe.title,
+        ingredients: recipe.ingredients,
+        directions: recipe.directions,
+        prep_time: recipe.prep_time,
+        image_url: recipe.image_url
+      };
+      axios
+        .patch(`/api/recipes/${recipe.id}`, params)
+        .then(response => {
+          console.log("Successfully Updated", response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+          // this.updateErrors = error.response.data.errors;
+        });
     }
   }
 };
