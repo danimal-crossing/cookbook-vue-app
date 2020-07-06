b7fb43ab8fdda1a07b2beacea7c749d69cd4a088
dani <dani.zaghian@gmail.com>
2020-07-06 11:58:45 -0700

    adds restful recipes index route

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/test">Test</router-link> |
       <router-link to="/signup">Signup</router-link> |
       <router-link to="/login">Login</router-link> |
-      <router-link to="/logout">Logout</router-link>
+      <router-link to="/logout">Logout</router-link> |
+      <router-link to="/recipes">All Recipes</router-link> 
     </div>
     <!-- individual components injected -->
     <router-view/>
---
+++ src/views/RecipesIndex.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="recipes-index">
+    <div v-for="recipe in recipes">
+      <h2>{{ recipe.title }}</h2>
+      <img v-bind:src="recipe.image_url" alt="">
+      <p>Prep time: {{ recipe.prep_time }}</p>
+      <button>More Info</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      recipes: []
+    };
+  },
+  created: function() {
+    axios.get("/api/recipes").then(response => {
+      console.log("All Recipes:", response.data);
+      this.recipes = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
