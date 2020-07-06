ddd97825df510f02d5d4f396e3478cb9892eb754
dani <dani.zaghian@gmail.com>
2020-07-06 12:33:38 -0700

    adds restful recipes new route

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
       <router-link to="/signup">Signup</router-link> |
       <router-link to="/login">Login</router-link> |
       <router-link to="/logout">Logout</router-link> |
-      <router-link to="/recipes">All Recipes</router-link> 
+      <router-link to="/recipes">All Recipes</router-link> |
+      <router-link to="/recipes/new">New Recipe</router-link> 
     </div>
     <!-- individual components injected -->
     <router-view/>
