3eb972c6cc48fe4401c53bc7fe3cd187a726dabc
dani <dani.zaghian@gmail.com>
2020-07-13 11:18:28 -0700

    adds mapbox map

---
+++ src/views/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="map">
+    <h1>Mapbox Map</h1>
+    <div id="map"></div>
+  </div>
+</template>
+
+<style>
+#map {
+  top: 0;
+  bottom: 0;
+  height: 600px;
+  width: 100%;
+}
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  mounted: function() {
+    mapboxgl.accessToken = "<your-access-token-here>";
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-122.4194, 37.7749], // starting position [lng, lat]
+      zoom: 13 // starting zoom
+    });
+  },
+  methods: {}
+};
+</script>
