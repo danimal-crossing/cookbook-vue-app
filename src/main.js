e40dfab65d8e76e50e98d2bc7ee9df46c0e783a5
dani <dani.zaghian@gmail.com>
2020-07-24 13:16:26 -0700

    adds heroku domain for backend rails app

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import axios from "axios"
 
 // default to using localhost:3000
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+  process.env.NODE_ENV === "development" ? "https://danimal-crossing-cookbook.herokuapp.com/" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
